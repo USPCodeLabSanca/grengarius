602b71fd351379a5c790b7b3a8f00c2266a8768c
jean sidharta <jeansidharta@gmail.com>
2019-09-18 16:05:48 -0700

    added a hamburguer

--- shared/components/navbar.js
+++ shared/components/navbar.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Navbar(){
   return (
     <header className='bg-gray top-0 w-full h-navbar'>
-      <div className='flex justify-center md:justify-start container h-full mx-auto'>
+      <div className='flex justify-between mx-2 md:mx-0 md:justify-start h-full'>
         <div><Link href="/index"><a className='flex items-center px-4'>
           <img src='/static/images/logo-navbar.svg' className='h-10' alt="Grengarius Games Logo" />
           <div className='text-4xl font-bold px-2'>
@@ -21,6 +21,9 @@ export default function Navbar(){
           <li className='px-4'><Link href="/about"><a>Sobre nós</a></Link></li>
           <li className='px-4'><Link href="/blog"><a>Blog</a></Link></li>
         </ul>
+        <div className="block md:hidden"> {/*Hamburguer!*/}
+          <object data="/static/images/hamburguer.svg" width="50" height="50"/>
+        </div>
       </div>
       <style jsx>{`
         .h-navbar {
